c44a66eaf1ec7b39748b20fbebc2f03e856a8d7d
hiru2k <mudannayakehiruni90@gmail.com>
2023-03-23 00:06:59 +0530

    user register pro pic layout

--- lib/pages/user_register_page.dart
+++ lib/pages/user_register_page.dart
@@ -3,6 +3,10 @@ import 'package:my_recipe/theme.dart';
 import 'package:my_recipe/widgets/gender_field.dart';
 import 'package:my_recipe/widgets/outlined_textfield.dart';
 
+const _avatarRadius = 75.0;
+const _pageMarginTop = 150.0;
+const _avatarMarginTop = _pageMarginTop - _avatarRadius;
+
 class UserRegisterPage extends StatelessWidget {
   static const path = '/user_register';
 
@@ -20,63 +24,75 @@ class UserRegisterPage extends StatelessWidget {
         backgroundColor: primaryColor,
       ),
       body: SingleChildScrollView(
-        child: Container(
-          color: primaryColor,
-          child: Column(
-            children: [
-              const SizedBox(height: 150),
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: pagePaddingHorizental,
-                ),
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  ),
-                ),
-                child: Column(
-                  children: [
-                    const SizedBox(height: 150),
-                    const OutlinedTextField(hintText: 'Full name'),
-                    const SizedBox(height: 30),
-                    const OutlinedTextField(hintText: 'Email'),
-                    const SizedBox(height: 30),
-                    const OutlinedTextField(hintText: 'Password'),
-                    const SizedBox(height: 30),
-                    const OutlinedTextField(hintText: 'Confirm Password'),
-                    const SizedBox(height: 30),
-                    const OutlinedTextField(hintText: 'Phone number'),
-                    const SizedBox(height: 30),
-                    const OutlinedTextField(hintText: 'Address'),
-                    const SizedBox(height: 30),
-                    const GenderField(),
-                    const SizedBox(height: 50),
-                    SizedBox(
-                      width: formButtonWidth,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        child: const Text('Sign Up'),
+        child: Stack(
+          children: [
+            Container(
+              color: primaryColor,
+              child: Column(
+                children: [
+                  const SizedBox(height: 150),
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: pagePaddingHorizental,
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
                       ),
                     ),
-                    const SizedBox(height: 20),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                    child: Column(
                       children: [
-                        const Text('Already have an account?'),
-                        TextButton(
-                          onPressed: () {},
-                          child: const Text('Sign In'),
+                        const SizedBox(height: 150),
+                        const OutlinedTextField(hintText: 'Full name'),
+                        const SizedBox(height: 30),
+                        const OutlinedTextField(hintText: 'Email'),
+                        const SizedBox(height: 30),
+                        const OutlinedTextField(hintText: 'Password'),
+                        const SizedBox(height: 30),
+                        const OutlinedTextField(hintText: 'Confirm password'),
+                        const SizedBox(height: 30),
+                        const OutlinedTextField(hintText: 'Phone number'),
+                        const SizedBox(height: 30),
+                        const OutlinedTextField(hintText: 'Address'),
+                        const SizedBox(height: 30),
+                        const GenderField(),
+                        const SizedBox(height: 50),
+                        SizedBox(
+                          width: formButtonWidth,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('Sign Up'),
+                          ),
                         ),
+                        const SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text('Already have an account?'),
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text('Sign In'),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 50),
                       ],
                     ),
-                    const SizedBox(height: 50),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            Positioned(
+              top: _avatarMarginTop,
+              left: MediaQuery.of(context).size.width / 2 - _avatarRadius,
+              child: const CircleAvatar(
+                radius: _avatarRadius,
+                backgroundImage: AssetImage('assets/user.png'),
+              ),
+            ),
+          ],
         ),
       ),
     );
