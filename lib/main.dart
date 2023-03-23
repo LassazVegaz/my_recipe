b4d649d152a5e413be00b149d9d4aa201dbb40a8
hiru2k <mudannayakehiruni90@gmail.com>
2023-03-23 20:11:13 +0530

    display list

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:my_recipe/pages/login_page.dart';
 import 'package:my_recipe/pages/sample_page.dart';
 import 'package:my_recipe/pages/user_register/user_register_page.dart';
 import 'package:my_recipe/pages/user_view_page.dart';
-import 'package:my_recipe/pages/users_list_page.dart';
+import 'package:my_recipe/pages/users_list/users_list_page.dart';
 import 'package:my_recipe/theme.dart';
 
 Future<void> main() async {
@@ -28,14 +28,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My Recipe',
       theme: themeData,
-      initialRoute: LoginPage.path,
+      initialRoute: UsersListPage.path,
       routes: {
         SampleScreen.path: (context) => const SampleScreen(),
         ChefRegisterPage.path: (context) => const ChefRegisterPage(),
         ChefsListPage.path: (context) => const ChefsListPage(),
         ChefViewPage.path: (context) => const ChefViewPage(),
         LoginPage.path: (context) => const LoginPage(),
-        UserRegisterPage.path: (context) => UserRegisterPage(),
+        UserRegisterPage.path: (context) => const UserRegisterPage(),
         UsersListPage.path: (context) => const UsersListPage(),
         UserViewPage.path: (context) => const UserViewPage(),
       },
