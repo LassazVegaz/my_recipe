715f56c4c01e64a0fc996b8fcf71fd9a2c59c04c
hiru2k <mudannayakehiruni90@gmail.com>
2023-03-23 15:58:22 +0530

    login page height responsive

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My Recipe',
       theme: themeData,
-      initialRoute: SampleScreen.path,
+      initialRoute: LoginPage.path,
       routes: {
         SampleScreen.path: (context) => const SampleScreen(),
         ChefRegisterPage.path: (context) => const ChefRegisterPage(),
