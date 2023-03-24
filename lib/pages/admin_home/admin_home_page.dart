1c847c862582f07126a06e0cb2d86c4cc258271b
LassazVegaz <lasindunuwanga@gmail.com>
2023-03-24 23:18:31 +0530

    admin finished

--- lib/pages/admin_home/admin_home_page.dart
+++ lib/pages/admin_home/admin_home_page.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:my_recipe/pages/admin_home/widgets/menu_item.dart';
+import 'package:my_recipe/pages/chefs_list_page.dart';
+import 'package:my_recipe/pages/login_page.dart';
+import 'package:my_recipe/pages/users_list/users_list_page.dart';
+import 'package:my_recipe/repositories/auth_repo.dart';
 
-class AdminHomePage extends StatelessWidget {
+final _authRepo = AuthRrepository.instance;
+
+class AdminHomePage extends StatefulWidget {
   static const path = '/admin_home';
 
   const AdminHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<AdminHomePage> createState() => _AdminHomePageState();
+}
+
+class _AdminHomePageState extends State<AdminHomePage> {
+  void _onLogoutPressed() async {
+    await _authRepo.signOut();
+
+    if (!mounted) return;
+
+    Navigator.of(context).pushNamedAndRemoveUntil(
+      LoginPage.path,
+      (route) => false,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,20 +39,26 @@ class AdminHomePage extends StatelessWidget {
       body: Column(
         children: [
           const SizedBox(height: 40),
-          const AdminMenuItem(
+          AdminMenuItem(
             title: 'View Chefs',
             image: 'assets/chef.jpg',
+            onPressed: () {
+              Navigator.of(context).pushNamed(ChefsListPage.path);
+            },
           ),
-          const AdminMenuItem(
+          AdminMenuItem(
             title: 'View Users',
             image: 'assets/users.jpg',
+            onPressed: () {
+              Navigator.of(context).pushNamed(UsersListPage.path);
+            },
           ),
           Expanded(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 OutlinedButton(
-                  onPressed: () {},
+                  onPressed: _onLogoutPressed,
                   child: const Text('Log Out'),
                 ),
                 const SizedBox(height: 10),
