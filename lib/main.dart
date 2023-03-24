c65e06f275ca53611b4305e884561a519714412d
LassazVegaz <lasindunuwanga@gmail.com>
2023-03-24 23:19:37 +0530

    main init route reset

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My Recipe',
       theme: themeData,
-      initialRoute: AdminHomePage.path,
+      initialRoute: LoginPage.path,
       routes: {
         SampleScreen.path: (context) => const SampleScreen(),
         ChefRegisterPage.path: (context) => const ChefRegisterPage(),
