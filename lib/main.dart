f4384f114b83995db358f54d5f9859eaf3a019e5
hiru2k <mudannayakehiruni90@gmail.com>
2023-03-24 09:38:22 +0530

    back to normal

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My Recipe',
       theme: themeData,
-      initialRoute: UserViewPage.path,
+      initialRoute: ChefRegisterPage.path,
       routes: {
         SampleScreen.path: (context) => const SampleScreen(),
         ChefRegisterPage.path: (context) => const ChefRegisterPage(),
