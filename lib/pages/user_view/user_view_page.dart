9cc82113c5e2abf1882be55ee070439f75931a04
hiru2k <mudannayakehiruni90@gmail.com>
2023-03-24 09:36:33 +0530

    delete finished

--- lib/pages/user_view/user_view_page.dart
+++ lib/pages/user_view/user_view_page.dart
@@ -9,7 +9,7 @@ import 'package:my_recipe/theme.dart';
 import 'package:my_recipe/widgets/user_fields.dart';
 
 // hard coded ID of the user to be displayed
-const _userId = 'U7hS6hf8KkeTpISEK9rp9sBAgch1';
+const _userId = 'O0QuKaTWBUYuoCO1w7Fe08VOMSj2';
 
 // margin top ratio of the fields container with respect to the screen height
 const _marginTopR = 0.47;
@@ -73,7 +73,27 @@ class _UserViewPageState extends State<UserViewPage> {
       stderr.writeln(e);
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
-          content: Text('Failed to update user. Please try again.'),
+          content: Text('Failed to update. Please try again.'),
+        ),
+      );
+    }
+  }
+
+  void _onDeletePressed() async {
+    try {
+      await _usersRepo.deleteUser(uid);
+      if (!mounted) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Account deleted successfully.'),
+        ),
+      );
+      Navigator.of(context).pop();
+    } catch (e) {
+      stderr.writeln(e);
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Failed to delete account. Please try again.'),
         ),
       );
     }
@@ -160,6 +180,7 @@ class _UserViewPageState extends State<UserViewPage> {
                         FormButtons(
                           buttonRadius: _fieldsContainerBorderRadius,
                           onUpdatePressed: _onUpdatePressed,
+                          onDeletePressed: _onDeletePressed,
                         ),
                       ],
                     ),
